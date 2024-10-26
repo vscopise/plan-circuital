660381c30001bc6cb1a82fbd86f8c148e63b0a71
Vicente Scópise <vscopise@gmail.com>
2024-10-25 21:34:32 -0300

    final 8

--- src/app/import/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ImportCsv } from "@/components";
-
-export default function ImportPage() {
-  return (
-    <main className="min-h-screen text-center py-2 px-2">
-      <ImportCsv />
-    </main>
-  );
-}
