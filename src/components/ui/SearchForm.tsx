a11ef293928883e284c7008b0a64b02aa9e3379a
Vicente Scópise <vscopise@gmail.com>
2024-10-25 21:45:21 -0300

    final 10

--- src/components/ui/SearchForm.tsx
+++ src/components/ui/SearchForm.tsx
@@ -13,16 +13,16 @@ export const SearchForm = () => {
     const onSubmit = async () => {
         //console.log({ serie, numero });
         if (!/^[a-zA-Z][a-zA-Z][a-zA-Z]+$/.test(serie)) {
-            setMessage('Error en la serie')
-            return
+            setMessage('Error en la serie');
+            return;
         }
 
         if (!/(?<!\d)\d{1,5}(?!\d)$/.test(numero)) {
-            setMessage('Error en el numero')
-            return
+            setMessage('Error en el numero');
+            return;
         }
-        setMessage('ok, cargando...')
-        const { ok, message } = await getCircuito({ serie: "", numero: "" })
+        setMessage('ok, cargando...');
+        const { ok, message } = await getCircuito({ serie: "", numero: "" });
         //console.log({message})
         if (ok) {
             setMessage(message);
